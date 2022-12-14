77096104c8f31befc5de140a3f0f8c6768e42930
Дмитрий Ерыганов <dnevvs@yandex.ru>
2022-12-14 10:35:11 +0300

    Final 6 1

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,7 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('group', 'text', 'image',)
-        
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
